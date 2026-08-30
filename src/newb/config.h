3a7e2b228e5680bfc772b6e4b72bacda7b459524
realcastafk <abdull201370@gmail.com>
2026-08-30 20:21:12 +0300

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -33,8 +33,8 @@
 //#define NL_EXPOSURE 1.3              // [toggle] 0.5 dark ~ 3.0 bright
 //#define NL_SATURATION 1.4            // [toggle] 0.0 grayscale ~ 4.0 super saturated
 #define NL_TINT                      // [toggle] enable light/dark tone tinting
-#define NL_TINT_LOW  vec3(0.9, 0.95, 1.0) // color tint for dark tone
-#define NL_TINT_HIGH vec3(1.02, 0.98, 0.95) // color tint for light tone
+#define NL_TINT_LOW  vec3(0.85,0.92,1.08) // color tint for dark tone
+#define NL_TINT_HIGH vec3(1.08,0.95,0.85) // color tint for light tone
 
 /* Lighting */
 #define NL_SUNLIGHT_INTENSITY   5.0  // 1.0 weak ~ 5.0 bright
@@ -50,7 +50,7 @@
 
 /* Sun/moon light color */
 #define NL_DAWN_SUNLIGHT_COL   vec3(1.0,0.4,0.1)
-#define NL_NOON_SUNLIGHT_COL   vec3(0.969, 0.894, 0.639)
+#define NL_NOON_SUNLIGHT_COL   vec3(0.980,0.922,0.733)
 #define NL_NIGHT_MOONLIGHT_COL vec3(0.01,0.03,0.2)
 
 /* Torch colors */
