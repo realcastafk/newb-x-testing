195769cd07255509aaa30725bb4665e0484ca9e9
realcastafk <abdull201370@gmail.com>
2026-09-04 23:43:29 +0300

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -50,7 +50,7 @@
 
 /* Sun/moon light color */
 #define NL_DAWN_SUNLIGHT_COL   vec3(1.0,0.4,0.1)
-#define NL_NOON_SUNLIGHT_COL   vec3(0.922,0.960,0.980)
+#define NL_NOON_SUNLIGHT_COL   vec3(1.0,0.969,0.922)
 #define NL_NIGHT_MOONLIGHT_COL vec3(0.01,0.03,0.2)
 
 /* Torch colors */
