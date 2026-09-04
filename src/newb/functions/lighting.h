ba98ade8148e3d3f2aae3fc678b1577ff222738c
realcastafk <abdull201370@gmail.com>
2026-09-04 19:25:13 +0300

    Update lighting.h

--- src/newb/functions/lighting.h
+++ src/newb/functions/lighting.h
@@ -111,8 +111,8 @@ vec3 nlLighting(
 
 	// darken at crevices
 	float col_max = max(COLOR.r, max(COLOR.g, COLOR.b));
-	if (col_max < 0.7) {
-		light *= 0.5;
+	if (col_max < 0.6) {
+		light *= 0.6;
 	};
 
   // brighten tree leaves
