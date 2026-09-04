f50b464781611fe79ab179364e2bae4ea84ad62f
realcastafk <abdull201370@gmail.com>
2026-09-04 15:55:53 +0300

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -28,7 +28,7 @@
 */
 
 /* Color correction */
-#define NL_TONEMAP_TYPE 3             // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
+#define NL_TONEMAP_TYPE 2             // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
 #define NL_GAMMA 1.3                  // 0.3 low ~ 2.0 high
 //#define NL_EXPOSURE 1.3              // [toggle] 0.5 dark ~ 3.0 bright
 //#define NL_SATURATION 1.4            // [toggle] 0.0 grayscale ~ 4.0 super saturated
@@ -37,7 +37,7 @@
 #define NL_TINT_HIGH vec3(1.08,0.95,0.85) // color tint for light tone
 
 /* Lighting */
-#define NL_SUNLIGHT_INTENSITY   4.4  // 1.0 weak ~ 5.0 bright
+#define NL_SUNLIGHT_INTENSITY   4.7  // 1.0 weak ~ 5.0 bright
 #define NL_TORCHLIGHT_INTENSITY 1.0  // 0.5 weak ~ 3.0 bright
 #define NL_SHADOW_INTENSITY     0.6  // 0.0 no shadow ~ 1.0 strong shadow
 #define NL_MIN_LIGHTING_BOOST   1.5  // 1.0 minimal lighting boost for dark areas ~ 3.0 brighter dark areas
@@ -50,7 +50,7 @@
 
 /* Sun/moon light color */
 #define NL_DAWN_SUNLIGHT_COL   vec3(1.0,0.4,0.1)
-#define NL_NOON_SUNLIGHT_COL   vec3(0.929,0.953,0.969)
+#define NL_NOON_SUNLIGHT_COL   vec3(0.882,0.945,0.988)
 #define NL_NIGHT_MOONLIGHT_COL vec3(0.01,0.03,0.2)
 
 /* Torch colors */
