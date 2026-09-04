81ea7342f4b9aae7049a1c7dd74208b1872d4c7b
realcastafk <abdull201370@gmail.com>
2026-09-04 15:29:03 +0300

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -28,7 +28,7 @@
 */
 
 /* Color correction */
-#define NL_TONEMAP_TYPE 4             // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
+#define NL_TONEMAP_TYPE 1             // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
 #define NL_GAMMA 1.3                  // 0.3 low ~ 2.0 high
 //#define NL_EXPOSURE 1.3              // [toggle] 0.5 dark ~ 3.0 bright
 //#define NL_SATURATION 1.4            // [toggle] 0.0 grayscale ~ 4.0 super saturated
@@ -37,7 +37,7 @@
 #define NL_TINT_HIGH vec3(1.08,0.95,0.85) // color tint for light tone
 
 /* Lighting */
-#define NL_SUNLIGHT_INTENSITY   5.4  // 1.0 weak ~ 5.0 bright
+#define NL_SUNLIGHT_INTENSITY   4.4  // 1.0 weak ~ 5.0 bright
 #define NL_TORCHLIGHT_INTENSITY 1.0  // 0.5 weak ~ 3.0 bright
 #define NL_SHADOW_INTENSITY     0.6  // 0.0 no shadow ~ 1.0 strong shadow
 #define NL_MIN_LIGHTING_BOOST   1.5  // 1.0 minimal lighting boost for dark areas ~ 3.0 brighter dark areas
