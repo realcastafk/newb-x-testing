50495dcd63d64812f9fe1e9b96cbeca1e6abfdaa
realcastafk <abdull201370@gmail.com>
2026-09-05 23:18:28 +0300

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -31,7 +31,7 @@
 #define NL_TONEMAP_TYPE 2             // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
 #define NL_GAMMA 1.33                  // 0.3 low ~ 2.0 high
 //#define NL_EXPOSURE 1.3              // [toggle] 0.5 dark ~ 3.0 bright
-#define NL_SATURATION 1.5            // [toggle] 0.0 grayscale ~ 4.0 super saturated
+#define NL_SATURATION 1.0            // [toggle] 0.0 grayscale ~ 4.0 super saturated
 //#define NL_TINT                      // [toggle] enable light/dark tone tinting
 #define NL_TINT_LOW  vec3(0.3,0.5,1.4) // color tint for dark tone
 #define NL_TINT_HIGH vec3(1.4,0.7,0.3) // color tint for light tone
